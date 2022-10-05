df3d2907517bb2475953622765eda3bc7b9ca5cb
Hafiz Caniago <hafizcode02@gmail.com>
2022-10-05 12:16:48 +0700

    make Todo

--- js/script.js
+++ js/script.js
@@ -27,6 +27,38 @@ const generateTodoObject = (id, task, detailTask, timestamp, isCompleted) => {
     };
 }
 
+const makeTodo = (todoObject) => {
+    const textTitle = document.createElement('h2');
+    textTitle.innerText = todoObject.task;
+
+    const textDetailTitle = document.createElement('h6');
+    textDetailTitle.innerHTML = `${todoObject.detailTask} <br>`;
+
+    const textTimestamp = document.createElement('p');
+    textTimestamp.innerText = todoObject.timestamp;
+
+    const textContainer = document.createElement('div');
+    textContainer.classList.add('inner');
+    textContainer.append(textTitle, textDetailTitle, textTimestamp);
+
+    const container = document.createElement('div');
+    container.classList.add('item', 'shadow');
+    container.append(textContainer);
+    container.setAttribute('id', `todo-${todoObject.id}`);
+
+    return container;
+}
+
+document.addEventListener(RENDER_EVENT, function () {
+    const uncompleteTODOList = document.getElementById('todos');
+    uncompleteTODOList.innerHTML = "";
+
+    for (const todoItem of todos) {
+        const todoElement = makeTodo(todoItem);
+        uncompleteTODOList.append(todoElement);
+    }
+});
+
 document.addEventListener('DOMContentLoaded', function () {
     const submitForm = document.getElementById('form');
     submitForm.addEventListener('submit', function (event) {
